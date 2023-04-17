4f12fa4de7459e0533bbf4ec5d6b09a1ceaf5d11
Nischal <nischalkafle123@gmail.com>
2023-04-17 17:29:17 +0545

    now changing into my custom code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} exact />
+        <Route path="/about" element={<About />} exact />
+        <Route path="/gallery" element={<Gallery />} exact />
+        <Route path="/contact" element={<Contact />} exact />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "./Hero";
+import Others from "./Others";
+import Footer from "./Footer";
+import Typed from 'react-typed';
+
+const Home = () => {
+  return (
+    <>
+    
+      <Hero/>
+      <Others />
+      <Footer/>
+    </>
+  );
+};
+
+export default Home;
